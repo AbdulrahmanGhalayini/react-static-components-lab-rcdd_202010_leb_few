ee106d501d4b86e43147e22c8fb1695e2d5fbee7
Abdulrahman Ghalayini <ghalayini.abdulrahman.17@gmail.com>
2021-01-03 23:12:18 +0000

    Automatically backed up by Learn

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import CatComponent from 'src/CatComponent.js';
-import GraceHopperQuoteComponent from 'src/GraceHopperQuoteComponent.js';
-import MouseComponent from 'src/MouseComponent.js';
+import CatComponent from 'src';
+import GraceHopperQuoteComponent from 'src';
+import MouseComponent from 'src';
 
 class App extends Component {
 	render() {
